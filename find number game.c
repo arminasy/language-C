418217b076974954b8e69282374cc1cf39a1ca60
arminasy <57625772+arminasy@users.noreply.github.com>
2021-02-18 23:06:11 +0400

    Update find number game.c
    Solved problem with new line.

--- find number game.c
+++ find number game.c
@@ -6,10 +6,10 @@
 int main()
 {
 
-    char Player[50];
+    char Player[100];
     printf("Hello what is your name: ");
-    scanf("%s", &Player);
-    printf("Hello %s lets play game, I want you to think number 0 to 100 and you will guess\n", Player);
+    scanf("%s", Player);
+    printf("\nHello %s lets play game:\n\nGuess the number 0 to 100 and you will win\n\n", Player);
     srand(time(NULL));
     int r = rand() % 100;
     int i = 0;
@@ -18,21 +18,22 @@ int main()
 
     while(r != i)
     {
+        printf("\n");
         printf("Check the number 0 to 100: ");
         scanf("%d", &i);
         if(i < r)
         {
-            printf("Checked Number is less than guessed\n");
+            printf("\nChecked Number is less than guessed\n");
             c++;
         }
         else if(i > r)
         {
-            printf("Checked number is greater than guessed\n");
+            printf("\nChecked number is greater than guessed\n");
             c++;
         }
         else if(i < 65);
 
         else
-            printf("Woowwwww you won : it took you %d tries\n", c);
+            printf("\nWoowwwww you won : it took you %d tries\n", c);
     }
 }
