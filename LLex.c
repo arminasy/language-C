b15d36332ea8b28be087f6c9ac41f3b57ed078d1
arminasy <57625772+arminasy@users.noreply.github.com>
2021-04-22 16:16:09 +0400

    Update LLex.c

--- LLex.c
+++ LLex.c
@@ -1,44 +1,56 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 #include <stdlib.h>
 
-typedef struct node
+typedef struct s_list
 {
-	int val;
-	struct node *next;
-}node;
+    char    *country;
+    size_t  population;
+    struct  s_list *next;
+}           t_list;
 
-void	printlist(node* n)
+void    print_list(t_list *n)
 {
-	while(n != NULL)
-	{
-		printf(" %d ", n->val);
-		n = n->next;
-	}
+    while(n != NULL)
+    {
+        printf("\nCountry name       : %s\nCountry population : %lu\n\n", n->country, n->population);
+        printf("%p\n", &n->country);
+        printf("%p\n", &n->population);
+        n = n->next;
+    }
 }
 
-int main(void)
+int     main(void)
 {
-	node *head;
-	node *second;
-	node *third;
-  node *fourth;
+    //in single LL the adresses are not sequence, That allow us add, delete elements
+    t_list *item1;
+    t_list *item2;
+    t_list *item3;
+    t_list *item4;
 
-	head = (node *)malloc(sizeof(node));
-	second = (node *)malloc(sizeof(node));
-	third = (node *)malloc(sizeof(node));
-  fourth = (node *)malloc(sizeof(node));
+    item1 = (t_list*)malloc(sizeof(t_list));
+    item2 = (t_list*)malloc(sizeof(t_list));
+    item3 = (t_list*)malloc(sizeof(t_list));
+    item4 = (t_list*)malloc(sizeof(t_list));
+    //-> address transfer
+    item1->country = "Armenia";
+    item1->population = 3100000;
+    item1->next = item2;
+    //*(item2) address transfer
+    //"->" and "." dont't have a difference if write (*p).dot
+    (*item2).country = "Georgia";
+    (*item2).population = 3970000;
+    (*item2).next = item3;
 
-	head->val = 1;
-	head->next = second;
+    item3->country = "Grecce";
+    item3->population = 9870000;
+    item3->next = item4;
 
-	second->val = 2;
-	second->next = third;
+    item4->country = "Iran";
+    item4->population = 78560000;
+    item4->next = NULL;
 
-	fourth->val = 4;
-	fourth->next = NULL;
-
-  third->val = 3;
-  third->next = fourth;
-
-	printlist(head);
+    print_list(item1);
+    //in fixed array we can't add element without using new array, which will be copied there...
 }
