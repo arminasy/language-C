c5dc9b7b2d4a4f826f6b3a75649aeaadcd252249
arminasy <57625772+arminasy@users.noreply.github.com>
2021-09-16 13:32:42 +0400

    w&r_2d_array

--- FILEc&w.c
+++ FILEc&w.c
@@ -4,29 +4,30 @@
 int main(void)
 {
     FILE *f = fopen("test1.txt", "w");
-    char *c;
-    int n;
-    c = (char *)malloc(sizeof(f));
+    int row = 0;
+    int col = 0;
+    int count = 0;
+    char (*c)[col] = malloc(sizeof(char) * (row * col));
 
-        scanf("%d", &n);
-            for(int i = 1; i < n; i++)
+        scanf("%d", &row);
+        scanf("%d", &col);
+    for(int i = 0; i < row; i++)
     {
-        if(i % 3 == 0 && i % 5 == 0)
-            fprintf(f, "%s", "FizzBuzz\n");
-        else if(i % 5 == 0)
-            fprintf(f, "%s","Buzz\n");
-        else if(i % 3 == 0)
-            fprintf(f, "%s", "Fizz\n");
-        else
-            fprintf(f ,"%d\n", i);
+        for(int j = 0; j < col; j++)
+        {
+            fprintf(f, "%c", '#');
+            count++;
+        }
+        fprintf(f, "%c", '\n');
     }
+    fprintf(f, "\n%d", count);
     fclose(f);
 
     f = fopen("test1.txt", "r");
         while(!feof(f))
         {
-            if(fscanf(f, "%s\n", c) > 0)
-                printf("%s\n", c);
+            if(fscanf(f, "%s\n", &c[row][col]) > 0)
+                printf("%s\n", &c[row][col]);
         }
 
     return (0);
